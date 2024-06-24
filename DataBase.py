d2c5a4d591d7b8756ab842a60fbcc245a0a3e474
Martin Grangé <martin.grange.1@ens.etsmtl.ca>
2024-06-24 14:52:00 -0400

    Changement chemin d'accès aux bases de données

--- DataBase.py
+++ DataBase.py
@@ -54,17 +54,17 @@ def vol_aeroport(aeroport, debut, fin):
 def FAA(DF):
     """Chargement et traitement de la base de donnée de la FAA"""
     """Base de donnee master"""
-    FAA_df = pd.read_csv('FAA/master.csv', sep=',', encoding='utf-8', low_memory=False, usecols=[2, 3, 33])
+    FAA_df = pd.read_csv('BaseDonnees/FAA/master.csv', sep=',', encoding='utf-8', low_memory=False, usecols=[2, 3, 33])
     FAA_df.rename(columns={'MODE S CODE HEX': 'icao24'}, inplace=True)
     FAA_df.rename(columns={'MFR MDL CODE': 'CODE'}, inplace=True)
     FAA_df.rename(columns={'ENG MFR MDL': 'CODE ENGINE'}, inplace=True)
     FAA_df['icao24'] = FAA_df['icao24'].str.strip()
 
     """Base de donnee modele"""
-    model_df = pd.read_csv('FAA/modele.csv', sep=',', encoding='utf-8', low_memory=False, usecols=[0, 2])
+    model_df = pd.read_csv('BaseDonnees/FAA/modele.csv', sep=',', encoding='utf-8', low_memory=False, usecols=[0, 2])
 
     """Base de donnee des moteurs"""
-    engine_df = pd.read_csv('FAA/engine.csv', sep=',', encoding='utf-8', low_memory=False, usecols=[0, 2])
+    engine_df = pd.read_csv('BaseDonnees/FAA/engine.csv', sep=',', encoding='utf-8', low_memory=False, usecols=[0, 2])
     engine_df.rename(columns={'CODE': 'CODE ENGINE'}, inplace=True)
     engine_df.rename(columns={'MODEL': 'ENGINE'}, inplace=True)
     DF['icao24'] = DF['icao24'].str.upper()
@@ -89,7 +89,7 @@ def airplane_traj(index):
     data = api.get_track_by_aircraft(icao24, t)
     return data.path
 def compagnie(DF):
-    compagnie_df = pd.read_csv('callsign.csv', sep=',', encoding='utf-8')
+    compagnie_df = pd.read_csv('BaseDonnees/callsign.csv', sep=',', encoding='utf-8')
     DF['callsign3'] = DF['callsign'].str[:3]
     DF = pd.merge(DF, compagnie_df, left_on='callsign3',right_on='callsign',how='left')
     DF = DF.drop(columns=['callsign3', 'callsign_y'])
