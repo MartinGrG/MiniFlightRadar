7e3371eb03196222d239d2395f076a6a5cca7aae
BuildTools <unconfigured@null.spigotmc.org>
2024-07-07 16:49:50 +0200

    Implémentation complète de la liste des émissions croisées modèle+moteurs
    Modification de la classe pdf pour qu'elle fonctionne avec les modifications apportées à la nouvelles liste des émissions
    
    Modifications mineures Interface.py

--- FlightRadar/PDFgenerator/Pdf_generateur.py
+++ FlightRadar/PDFgenerator/Pdf_generateur.py
@@ -2,11 +2,13 @@
 from fpdf import FPDF
 import matplotlib.pyplot as plt
 
-
 class Pdf(FPDF):
     titre = "Titre par défaut"
-    infos_vol = ["index","callsign","AAAA","AAAA","icao24","aaaa/mm/jj 13:34:se","aaaa/mm/jj 15:23:se","compagnie","codeModel","codeEngine","model", "numberEngine", "modelReduit","modelEngine","uid","1000"]
-    infos_emission = [["engine1_test","engine2_test","engine3_test","CF34-8C5","engine5"],[0.1,0.2,0.3,0.7,1.2]]
+    infos_vol = ["index", "callsign", "AAAA", "AAAA", "icao24", "aaaa/mm/jj 13:34:se", "aaaa/mm/jj 15:23:se",
+                 "compagnie", "codeModel", "codeEngine", "model", "numberEngine", "modelReduit", "modelEngine", "uid",
+                 "1000"]
+    infos_emission = [['CL6'], [["engine1_test", "engine2_test", "engine3_test", "CF34-8C5", "engine5"]],
+                      [[0.1, 0.2, 0.3, 0.7, 1.2]]]
     graphique_emission = ""
     map_chemin = ""
 
@@ -28,11 +30,11 @@ class Pdf(FPDF):
         plt.xlabel("Noms des moteurs")
         plt.ylabel("Valeurs d'émission CO2 par personne (t)")
         graph_bar1 = plt.bar(x[0], y[0], color="green", label="Moteur d'origine")
-        graph_bar2 = plt.bar(x[1:len(x)], y[1:len(y)], color="gray",label="Autres moteurs")
+        graph_bar2 = plt.bar(x[1:len(x)], y[1:len(y)], color="gray", label="Autres moteurs")
         plt.bar_label(graph_bar1, labels=[y[0]])
         plt.bar_label(graph_bar2, labels=y[1:len(y)])
 
-        #plt.bar_label(graph_bar1.container[1], labels=y[1:len(y)])
+        # plt.bar_label(graph_bar1.container[1], labels=y[1:len(y)])
         plt.xticks(rotation=15)
         plt.legend()
         plt.savefig("Interface/graphique_emission.png")
@@ -67,20 +69,33 @@ class Pdf(FPDF):
         diff = heure2 - heure1
         diff_h = int(diff)
         diff_m = round((diff - int(diff)) * 60)
-        return str(diff_h) + 'h' + str(diff_m) +'min'
+        return str(diff_h) + 'h' + str(diff_m) + 'min'
 
     def ajouter_tableau(self, donnees):
         taille_max = 45
-        self.set_font('helvetica', 'B', 10)
-        self.cell(taille_max, 9, str(donnees[0][0]), 1, align='C')
-        self.cell(taille_max, 9, str(donnees[1][0]), 1, align='C')
-        self.ln()
         self.set_font('helvetica', '', 11)
 
-        for i in range(1,len(donnees[0])):
-            self.cell(taille_max, 9, str(donnees[0][i]), 1, align='C')
-            self.cell(taille_max, 9, str(donnees[1][i]), 1, align='C')
+        for i in range(len(donnees[0])):
+            self.cell(taille_max * 2, 9, str(donnees[0][i]), 1, align='C')
             self.ln()
+            self.set_font('helvetica', 'B', 11)
+            self.cell(taille_max, 9, "moteurs", 1, align='C')
+            self.cell(taille_max, 9, "émission t(CO2)/pers", 1, align='C')
+            self.set_font('helvetica', '', 11)
+            self.ln()
+            for j in range(len(donnees[1][i])):
+                if i == 0 and j == 0:
+                    self.set_text_color(0, 150, 0)
+                    self.cell(taille_max, 9, str(donnees[1][i][j]), 1, align='C')
+                    self.cell(taille_max, 9, str(donnees[2][i][j]), 1, align='C')
+                    self.ln()
+                    self.set_text_color(0, 0, 0)
+
+
+                else:
+                    self.cell(taille_max, 9, str(donnees[1][i][j]), 1, align='C')
+                    self.cell(taille_max, 9, str(donnees[2][i][j]), 1, align='C')
+                    self.ln()
         self.ln()
 
     def ajouter_separateur(self):
@@ -92,10 +107,10 @@ class Pdf(FPDF):
         # En-tête et pied de page personnalisés
         self.alias_nb_pages()
         self.set_auto_page_break(auto=True)
-        
+
         # Titre principal
         self.set_font('helvetica', 'B', 16)
-        self.cell(self.get_string_width('FICHE DE VOL :')+2, 10, 'FICHE DE VOL :', 0)
+        self.cell(self.get_string_width('FICHE DE VOL :') + 2, 10, 'FICHE DE VOL :', 0)
 
         self.set_font('helvetica', '', 12)
         self.cell(0, 10, text=self.infos_vol[7] + ' ' + self.infos_vol[1], border=0)
@@ -106,39 +121,45 @@ class Pdf(FPDF):
         self.ln(10)
 
         # Section d'information
-        long_page = self.w-12.7-12.7
+        long_page = self.w - 12.7 - 12.7
 
         self.set_font('helvetica', '', 11)
         x_i = self.x
         y_i = self.y
-        self.cell(long_page-self.get_string_width(self.infos_vol[9])-10, 10, text=self.infos_vol[10]+' ('+self.infos_vol[12]+')', align='L')
+        self.cell(long_page - self.get_string_width(self.infos_vol[9]) - 10, 10,
+                  text=self.infos_vol[10] + ' (' + self.infos_vol[12] + ')', align='L')
         self.cell(self.get_string_width('Moteur'), h=10, text='Moteur :', align='L')
         self.ln(5)
         self.set_font('helvetica', 'B', 11)
-        self.cell(self.get_string_width(self.infos_vol[3]+' : '), 10, text=self.infos_vol[3]+' : ')
+        self.cell(self.get_string_width(self.infos_vol[3] + ' : '), 10, text=self.infos_vol[3] + ' : ')
         self.set_font('helvetica', '', 11)
-        self.cell(long_page-self.get_string_width(self.infos_vol[9])-self.get_string_width(self.infos_vol[3]+' : ')-10, 10, text=self.infos_vol[5][11:16])
+        self.cell(long_page - self.get_string_width(self.infos_vol[9]) - self.get_string_width(
+            self.infos_vol[3] + ' : ') - 10, 10, text=self.infos_vol[5][11:16])
         self.cell(self.get_string_width(self.infos_vol[9]), h=10, text=self.infos_vol[9], align='L')
-        x_f = long_page+12.7
+        x_f = long_page + 12.7
         self.ln(5)
         self.set_font('helvetica', 'B', 11)
         self.cell(self.get_string_width(self.infos_vol[2] + ' : '), 10, text=self.infos_vol[2] + ' : ')
         self.set_font('helvetica', '', 11)
-        self.cell(long_page-self.get_string_width(self.infos_vol[9])-self.get_string_width(self.infos_vol[2]+' : ')-10, 10, text=self.infos_vol[6][11:16])
+        self.cell(long_page - self.get_string_width(self.infos_vol[9]) - self.get_string_width(
+            self.infos_vol[2] + ' : ') - 10, 10, text=self.infos_vol[6][11:16])
         self.cell(self.get_string_width(self.infos_vol[-3]), h=10, text=self.infos_vol[-3], align='L')
         self.ln(5)
-        self.cell(long_page-self.get_string_width(self.infos_vol[9])-10, 10, text='Distance parcourue : '+str(round(float(self.infos_vol[-1]), 1))+'km')
+        self.cell(long_page - self.get_string_width(self.infos_vol[9]) - 10, 10,
+                  text='Distance parcourue : ' + str(round(float(self.infos_vol[-1]), 1)) + 'km')
         self.ln(5)
-        self.cell(long_page-self.get_string_width(self.infos_vol[9])-10, 10, text=f'Temps de vol : {self.calcule_duree(self.infos_vol[5],self.infos_vol[6])}')
+        self.cell(long_page - self.get_string_width(self.infos_vol[9]) - 10, 10,
+                  text=f'Temps de vol : {self.calcule_duree(self.infos_vol[5], self.infos_vol[6])}')
 
         self.ln()
         y_f = self.y
-        self.cell(self.get_string_width('icao24')+3, 10, 'icao24', border=1)
+        self.cell(self.get_string_width('icao24') + 3, 10, 'icao24', border=1)
         self.cell(0, 10, text=self.infos_vol[4], border=1, align='L')
 
         self.ln(10)
-        self.rect(x_i, y_i, x_f-x_i, y_f-y_i)
-        self.rect(x_i + long_page-self.get_string_width(self.infos_vol[9])-10, y_i, x_f - x_i - (long_page-self.get_string_width(self.infos_vol[9])-10), y_f - y_i)
+        self.rect(x_i, y_i, x_f - x_i, y_f - y_i)
+        self.rect(x_i + long_page - self.get_string_width(self.infos_vol[9]) - 10, y_i,
+                  x_f - x_i - (long_page - self.get_string_width(self.infos_vol[9]) - 10), y_f - y_i)
 
         self.ln(5)
         # Image
@@ -146,10 +167,10 @@ class Pdf(FPDF):
         x_i = self.x
         y_i = self.y
 
-        self.ajouter_image(self.map_chemin, 12.7+long_page/8, None, 3*long_page/4)
+        self.ajouter_image(self.map_chemin, 12.7 + long_page / 8, None, 3 * long_page / 4)
         y_f = self.y
         self.set_line_width(0.5)  # Épaisseur de trait de 2 points
-        self.rect(x_i+long_page/8, y_i, 3*long_page/4, y_f-y_i)
+        self.rect(x_i + long_page / 8, y_i, 3 * long_page / 4, y_f - y_i)
         self.set_line_width(0.2)  # Épaisseur de trait de 2 points
 
         # Séparateur
@@ -158,19 +179,18 @@ class Pdf(FPDF):
         # Nouveau titre
         self.titre_chapitre('Emission CO2 du vol (en t par passager)')
 
-
         # Image
-        self.ajouter_image(self.generer_graphique(), self.w-108, y_i, 105)
+        # self.ajouter_image(self.generer_graphique(), self.w-108, y_i, 105)
 
         self.ln(5)
         # Tableau
         donnees = self.infos_emission
-        donnees[0].insert(0, 'Modèles de moteurs')
-        donnees[1].insert(0, 'Emissions CO2 (t/prs)')
+        # donnees[0].insert(0, 'Modèles de moteurs')
+        # donnees[1].insert(0, 'Emissions CO2 (t/prs)')
         self.ajouter_tableau(donnees)
 
         self.set_line_width(1)  # Épaisseur de trait de 2 points
-        self.rect(4, 4, self.w-8, self.h-8)
+        self.rect(4, 4, self.w - 8, self.h - 8)
 
         # Enregistrer le PDF
         self.output("Interface/Compte_rendu.pdf")
