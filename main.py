9bb5f1bd6a1da543445c34adca219d5808b53b9d
200~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~ <louannevillette2000@gmail.com>
2024-07-08 18:42:08 -0400

    Correction de warnings

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from FlightRadar.Interface import Interface
 from FlightRadar.DataBase import telechargementBaseDonnee
 
 # Appel des fonctions pour télécharger les bases de données fournies par la FAA et l'EASA
-telechargementBaseDonnee.base_de_donnees_FAA()
-telechargementBaseDonnee.base_de_donnees_EASA()
+telechargementBaseDonnee.base_de_donnees_faa()
+telechargementBaseDonnee.base_de_donnees_easa()
 
 # On crée l'objet Interface
 interface = Interface.Interface()
